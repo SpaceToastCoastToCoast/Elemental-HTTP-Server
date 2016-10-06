ed10566931a54041e56813bd60f3beacf5685c48
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-10-06 13:53:57 -1000

    moved html generation to a submodule

--- server.js
+++ server.js
@@ -111,27 +111,14 @@ function incomingPost(request, response, body) {
   });
   console.log(pairedData);
   //now verify the data
-  if(!pairedData.hasOwnProperty('elementName')) {
+  if(!pairedData.hasOwnProperty('elementName') || !pairedData.hasOwnProperty('elementSymbol') ||
+    !pairedData.hasOwnProperty('elementAtomicNumber') || !pairedData.hasOwnProperty('elementDescription')) {
     response.statusCode = 400;
     response.end();
     return;
   }
   pairedData.elementDescription = pairedData.elementDescription.split('+').join(' ');
-  let generatedPage = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>The Elements - ${pairedData.elementName}</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <h1>${pairedData.elementName}</h1>
-  <h2>${pairedData.elementSymbol}</h2>
-  <h3>Atomic number ${pairedData.elementAtomicNumber}</h3>
-  <p>${pairedData.elementDescription}</p>
-  <p><a href="/">back</a></p>
-</body>
-</html>`;
+  let generatedPage = makeElementPage(pairedData);
   //now write to fs
   fs.writeFile(`./public/${pairedData.elementName.toLowerCase()}.html`, generatedPage, (err) => {
     if (err) {
@@ -140,4 +127,22 @@ function incomingPost(request, response, body) {
     console.log('Created file');
   });
   response.end();
+}
+
+function makeElementPage(elementObject) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - ${elementObject.elementName}</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>${elementObject.elementName}</h1>
+  <h2>${elementObject.elementSymbol}</h2>
+  <h3>Atomic number ${elementObject.elementAtomicNumber}</h3>
+  <p>${elementObject.elementDescription}</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`;
 }
